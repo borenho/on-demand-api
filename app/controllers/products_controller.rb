f9d72e97958733e7f7544fd201519d0a378969b0
borenho <borecollins@gmail.com>
2017-12-22 20:30:35 +0300

    CRUD products and make their tests pass

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,47 @@
 class ProductsController < ApplicationController
+    before_action :set_merchant
+    before_action :set_product, only: [:show, :update, :destroy]
+
+    # GET /merchants/:merchant_id/products
+    def index
+        json_response(@merchant.products)
+    end
+
+    # GET /merchants/:merchant_id/products/:product_id
+    def show
+        json_response(@product)
+    end
+
+    # POST /merchants/:merchant_id/products
+    def create
+        @merchant.products.create!(product_params)
+        json_response(@merchant, :created)  # TODO return the created product, not the merchant
+    end
+
+    # PUT /merchants/:merchant_id/products/:product_id
+    def update
+        @product.update(product_params)
+        head :no_content
+    end
+
+    # DELETE /merchants/:merchant_id/products/:product_id
+    def destroy
+        @product.destroy
+        head :no_content
+    end
+
+
+    private
+
+    def product_params
+        params.permit(:name, :price)
+    end
+
+    def set_merchant
+        @merchant = Merchant.find(params[:merchant_id])
+    end
+
+    def set_product
+        @product = @merchant.products.find_by!(id: params[:id]) if @merchant
+    end
 end
---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,134 @@
+require 'rails_helper'
+
+RSpec.describe 'Products API' do
+    # Initialize test data
+    let!(:merchant) { create(:merchant) }
+    let!(:products) { create_list(:product, 20, merchant_id: merchant.id) }
+    let(:merchant_id) { merchant.id }
+    let(:product_id) { products.first.id }
+
+    # GET /merchants/:merchant_id/products
+    describe 'GET /merchants/:merchant_id/products' do
+        before { get "/merchants/#{merchant_id}/products" }
+
+        context 'when merchant exists' do
+            it 'returns status code 200' do
+                expect(response).to have_http_status(200)
+            end
+
+            it "returns all the merchant's products" do
+                expect(json.size).to eq(20)
+            end
+        end
+
+        context 'when merchant does not exist' do
+            let(:merchant_id) { 0 }
+
+            it 'return status code 404' do
+                expect(response).to have_http_status(404)
+            end
+
+            it "returns a not found error message" do
+                expect(response.body).to match(/Couldn't find Merchant/)
+            end
+        end
+    end
+
+    # GET /merchants/:merchant_id/products/:product_id
+    describe 'GET /merchants/:merchant_id/products/product_id' do
+        before { get "/merchants/#{merchant_id}/products/#{product_id}" }
+
+        context 'when product exists' do
+            it 'return status code 200' do
+                expect(response).to have_http_status(200)
+            end
+
+            it "returns the product" do
+                expect(json['id']).to eq(product_id)
+            end
+        end
+
+        context 'when product does not exist' do
+            let(:product_id) { 0 }
+
+            it 'return status code 404' do
+                expect(response).to have_http_status(404)
+            end
+
+            it "returns a not found error message" do
+                expect(response.body).to match(/Couldn't find Product/)
+            end
+        end
+    end
+
+    # POST /merchants/:merchant_id/products
+    describe 'POST /merchants/:merchant_id/products' do
+        let(:valid_attributes) { { name: '3 Bedroom', price: 20000 } }
+
+        context 'when request attributes are valid' do
+            before { post "/merchants/#{merchant_id}/products", params: valid_attributes }
+
+            it 'return status code 201' do
+                expect(response).to have_http_status(201)
+            end
+
+            it "returns the created product" do
+                get "/merchants/#{merchant_id}/products/#{product_id}"
+
+                expect(json['id']).to eq(product_id)
+            end
+        end
+
+        context 'when the request is invalid' do
+            before { post "/merchants/#{merchant_id}/products", params: {} }
+
+            it 'return status code 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it "returns a validation error message" do
+                expect(response.body).to match(/Validation failed: Name can't be blank/)
+            end
+        end
+    end
+
+    # PUT /merchants/:merchant_id/products/:product_id
+    describe 'PUT /merchants/:merchant_id/products/:product_id' do
+        let(:valid_attributes) { { name: 'Air Bnb', price: 50000 } }
+
+        before { put "/merchants/#{merchant_id}/products/#{product_id}", params: valid_attributes }
+
+        context 'when item exists' do
+
+            it 'return status code 204' do
+                expect(response).to have_http_status(204)
+            end
+
+            it 'updates the product' do
+                updated_product = Product.find(product_id)
+                expect(updated_product.name).to match(/Air Bnb/)
+            end
+        end
+
+        context 'when the product does not exist' do
+            let(:product_id) { 0 }
+
+            it 'return status code 404' do
+                expect(response).to have_http_status(404)
+            end
+
+            it "returns a not found error message" do
+                expect(response.body).to match(/Couldn't find Product/)
+            end
+        end
+    end
+
+    # DELETE /merchants/:merchant_id/products/:product_id
+    describe 'DELETE /merchants/:merchant_id/products/:product_id' do
+        before { delete "/merchants/#{merchant_id}/products/#{product_id}" }
+
+        it 'returns a status code 204' do
+            expect(response).to have_http_status(204)
+        end
+    end
+end
