ecead43e9ed8f3993e39ff2d5da0fcdce96c64ad
borenho <borecollins@gmail.com>
2018-01-03 22:06:34 +0300

    Add TODOs

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,5 +1,5 @@
 module Response
-    # Define the json_response helper which responds with JSON and an HTTP status code (200 by default)
+    # Define the json_response helper which responds with JSON and an HTTP status code (defaults to 200 ok)
     def json_response(object, status = :ok)
         render json: object, status: status
     end
